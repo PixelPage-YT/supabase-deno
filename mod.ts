6389b8dc97b763d17966ec96297fef8f33cb001c
Kloetzchengrafikk <pascal.adk@icloud.com>
2022-02-20 12:21:13 +0100

    Better Infrastructure

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 export {supabaseClient} from "./src/client.ts"
 export {supabaseTable} from "./src/table.ts"
 export {supabaseTableItems} from "./src/tableItems.ts"
+export {supabaseClientTables} from "./src/clienttables.ts"
