56b47b36343f77183692464498e3925df0cbc3c1
Kloetzchengrafikk <pascal.adk@icloud.com>
2022-02-20 16:07:25 +0100

    Added item edit

--- src/tableItems.ts
+++ src/tableItems.ts
@@ -24,18 +24,36 @@ export class supabaseTableItems{
 
     async delete(matchname:string,matchvalue:string|number|object|boolean){
         let res = await fetch(this.client.url + "rest/v1/" + this.tableName + "?" + matchname + "=eq." + matchvalue,{
-                method:'DELETE',
-                headers: {
-                    'Content-Type':'application/json',
-                    "apikey": this.client.anonkey
-                },
-            }
-        )
+            method:'DELETE',
+            headers: {
+                'Content-Type':'application/json',
+                "apikey": this.client.anonkey
+            },
+        })
         return await res.text()
     }
 
     async get(matchname:string,matchvalue:string|number|object|boolean){
         let res = await fetch(this.client.url + "rest/v1/" + this.tableName + "?" + matchname + "=eq." + matchvalue +  "&select=*", {headers:{"apikey": this.client.anonkey}})
         return await res.json()
+    }
+
+    async edit(matchname:string,matchvalue:string|number|object|boolean,newitem:object){
+        let res = await fetch(this.client.url + "rest/v1/" + this.tableName + "?" + matchname + "=eq." + matchvalue,{
+            method:'DELETE',
+            headers: {
+                'Content-Type':'application/json',
+                "apikey": this.client.anonkey
+            },
+        })
+        let res1 = await fetch(this.client.url + "rest/v1/" + this.tableName, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                "apikey":this.client.anonkey
+            },
+            body: JSON.stringify(newitem)
+        })
+        return await res1.text()
     }
 }
